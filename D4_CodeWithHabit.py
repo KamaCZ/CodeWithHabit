7acfaf2370f5386a2fe13bd80608a5b728f5de10
KamaCZ <kama.klemsa@gmail.com>
2021-01-01 19:03:04 +0100

    I added day 4 file

---
+++ D4_CodeWithHabit.py
@@ -0,0 +1,43 @@
+"""
+Data structures
+1) strings
+2) lists
+3) dictionaries 
+4) tuples
+
+What are strings?
+How we write strings?
+What can we do with strings?
+Creating a program using some of the most important string operations
+"""
+
+"Kamil"
+
+"""
+print("Kamil")
+print('Kamil')
+name = "Kamil"
+surname = "Klemsa"
+print(name + " " + surname)
+print(name * 10)
+print(len("My name is Kamil. How  about you?"))
+
+word = "fine"
+"fnei"
+print(word[1])
+print(word[1:4])
+print(name.lower())
+"""
+
+
+# Mad Libs Game
+
+animal = (input("Write your favourite animal: "))
+activity = (input("Write your favourite activity: "))
+verb = (input("Write any verb: "))
+
+print("My favorite animal is " + animal)
+print(activity + " is its favourite activity")
+print("But now I want to " + verb + " it.")
+
+
