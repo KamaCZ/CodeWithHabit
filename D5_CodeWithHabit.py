1ae8c1502ad483031be4812ead01dfcd67ed3449
KamaCZ <kama.klemsa@gmail.com>
2021-01-02 19:08:39 +0100

    The D5 modified

--- D5_CodeWithHabit.py
+++ D5_CodeWithHabit.py
@@ -1,1 +1,10 @@
-print("Kamil")
+# rewriting for loop into while loop
+
+for i in range(5,0,-1):
+    print("Kamil " + str(i))
+print()
+
+i = 1
+while i < 6:
+    print("Kamil " + str(i))
+    i = i + 1
