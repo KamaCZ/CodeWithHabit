69309bc54dec4fc074fdb9b3cb468c4a2ab0dd36
KamaCZ <kama.klemsa@gmail.com>
2021-01-19 10:51:53 +0100

    D22: TIC TAC TOE GAME, part 1

---
+++ D22_CodeWithHabit.py
@@ -0,0 +1,28 @@
+# TIC TAC TOE GAME
+
+# Creating a playing board
+board = ['-', '-', '-',
+         '-', '-', '-',
+         '-', '-', '-']
+print(board)
+
+# defining a function that will display the playing board after each turn
+def display_board():
+    print("| " + board[0] + " | " + board[1] + " | " + board[2] + " |")
+    print("| " + board[3] + " | " + board[4] + " | " + board[5] + " |")
+    print("| " + board[6] + " | " + board[7] + " | " + board[8] + " |")
+    
+
+display_board()
+
+# defining a function that will play the game
+def play_game():
+    # display initial board
+    def display_board()
+# to be finished next time
+
+# Videos to watch: https://www.youtube.com/watch?v=8ext9G7xspg&t=340s
+# https://www.youtube.com/watch?v=4F2m91eKmts&list=PL-J2q3Ga50oMG2g_gCL66SVAK8VJzqt7t&index=6
+
+
+
