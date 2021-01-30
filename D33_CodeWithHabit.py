858548ed3a9bf50474d6936eacb37d9273df9751
KamaCZ <kama.klemsa@gmail.com>
2021-01-30 12:51:53 +0100

    D33: TIC TAC TOE, yes finished in 30 min but one small bug

--- D33_CodeWithHabit.py
+++ D33_CodeWithHabit.py
@@ -1,6 +1,6 @@
-# Tic Tac Toe Game
+
 print()
-print("Hi, this is TIC TAC TOE game. Enjoy!")
+print("This is TIC TAC TOE GAME. Enjoy!")
 print()
 
 board = ["-","-","-","-","-","-","-","-","-",]
@@ -16,11 +16,11 @@ def display_board():
     print("| " + board[3] + " | " + board[4] + " | " + board[5] + " |")
     print("| " + board[6] + " | " + board[7] + " | " + board[8] + " |")
 
+
 def play_game():
     display_board()
-    
-    while game_still_going:
 
+    while game_still_going:
         handle_turn(current_player)
 
         check_win()
@@ -28,17 +28,16 @@ def play_game():
         check_tie()
 
         flip_player()
-    return
 
     if winner == "X" or winner == "O":
         print(winner + " won.")
-    else: 
-        print("It was a tie.")
+    else:
+        print("Tie")
 
 
 
 def handle_turn(player):
-    print(player + "'s turn.")
+    print(player + "'s turn")
     position = input("Enter number from 1 to 9: ")
     position = int(position) - 1
     board[position] = player
@@ -46,41 +45,47 @@ def handle_turn(player):
     return
 
 def check_win():
-    while game_still_going:
+    global winner 
+    row_winner = check_rows()
+    column_winner = check_columns()
+    diagonal_winner = check_diagonals()
 
-        row_winner = check_rows()
-        column_winner = check_columns()
-        diagonal_winner = check_diagonals()
-        if row_winner:
-            winner = row_winner
-        if column_winner:
-            winner = column_winner 
-        if diagonal_winner:
-            winner = diagonal_winner
-        return
+    if row_winner:
+        winner = row_winner
+    elif column_winner:
+        winner = column_winner
+    elif diagonal_winner:
+        winner = diagonal_winner
+    else:
+        winner = None
+    return
 
 def check_rows():
     global game_still_going
-    row_1 = board[0] == board[1] == board[2] != "_"
-    row_2 = board[3] == board[4] == board[5] != "_"
-    row_3 = board[6] == board[7] == board[8] != "_"
+    row_1 = board[0] == board[1] == board[2] != "-"
+    row_2 = board[3] == board[4] == board[5] != "-"
+    row_3 = board[6] == board[7] == board[8] != "-"
+
     if row_1 or row_2 or row_3:
-        game_still_going = False
+        game_still_going = True
+
     if row_1:
         return board[0]
     if row_2:
         return board[3]
     if row_3:
         return board[6]
-    
+    return
 
 def check_columns():
     global game_still_going
-    column_1 = board[0] == board[3] == board[6] != "_"
-    column_2 = board[1] == board[4] == board[5] != "_"
-    column_3 = board[2] == board[7] == board[8] != "_"
+    column_1 = board[0] == board[3] == board[6] != "-"
+    column_2 = board[1] == board[4] == board[7] != "-"
+    column_3 = board[2] == board[5] == board[8] != "-"
+
     if column_1 or column_2 or column_3:
-        game_still_going = False
+        game_still_going = True
+
     if column_1:
         return board[0]
     if column_2:
@@ -91,22 +96,21 @@ def check_columns():
 
 def check_diagonals():
     global game_still_going
-    diagonal_1 = board[0] == board[4] == board[8] != "_"
-    diagonal_2 = board[2] == board[4] == board[6] != "_"
-    
+    diagonal_1 = board[0] == board[4] == board[8] != "-"
+    diagonal_2 = board[2] == board[4] == board[6] != "-"
+
     if diagonal_1 or diagonal_2:
-        game_still_going = False
+        game_still_going = True
+
     if diagonal_1:
         return board[0]
     if diagonal_2:
         return board[2]
-
     return
 
 
 
 def check_tie():
-    global game_still_going 
     if "-" not in board:
         game_still_going = False
     return
@@ -114,15 +118,14 @@ def check_tie():
 def flip_player():
     global current_player
     if current_player == "X":
-        current_player = "O"
+        curren_player = "O"
     elif current_player == "O":
         current_player = "X"
-
     return
 
 
+    
 
 play_game()
 
-
-# made a bug somewhere, but dont know where. Gonna check tomorrow.
+# one bug must check it tomorrow
