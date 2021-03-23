2a7d2e73e6c71b52a2a47609d003cba6abf31e4d
KamaCZ <kama.klemsa@gmail.com>
2021-03-23 10:03:42 +0100

    D82: arguments, *args

---
+++ D82_CodeWithHabit.py
@@ -0,0 +1,27 @@
+# *args and **kwargs
+# two functional parameters
+# *args = star args = stands for arguments
+# **kwargs = double star kwargs = stands for k-word arguments
+
+
+def myfunc(a,b):
+    # returns 5% of the sum a and b
+    return sum((a,b))* 0.05
+
+print(myfunc(40,60))
+
+# in case we want to pass more arguments:
+
+def myfunc(a,b,c=0,d=0,e=0):
+    # returns 5% of the sum a and b
+    return sum((a,b,c,d,e))* 0.05
+
+print(myfunc(40,60,100))
+
+def myfunc(*args): # passing * args argument allows me to deal with it like a tuple of arguments
+    return sum(args) * 0.05
+
+print(myfunc(40,60,200))
+
+def myfunc(*spam): # note: we can use any word after the *
+    return sum(spam)
